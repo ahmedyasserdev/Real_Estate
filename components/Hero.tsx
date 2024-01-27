5efffec362f952119c588f3581c66d8636ab7978
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-01-27 10:35:24 +0200

    added hero

---
+++ components/Hero.tsx
@@ -0,0 +1,23 @@
+
+const Hero = () => {
+  return (
+    <section className='pt-5  ' >
+        <div className="container   hero">
+          <div className="flex-start p-12" >
+              <article className = " flex flex-col gap-5  text-center  md:text-start w-full " >
+                  <h1 className=" bold-36 lg:bold-60 2xl:bold-88 " >Easy way to find a
+           <br /> rfect property</h1>
+
+        <p className="text-grey-700  regular-20 md:regular-24 md:max-w-[550px] ">
+        We provide a complete service for the sale,
+purchase or rental of real estate.
+        </p>
+
+              </article>
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
